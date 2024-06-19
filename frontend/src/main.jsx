dd39353fd3b7e4b6afd908185664c45ff59ed1b5
vikask0199 <vikasku0199@gmail.com>
2024-06-20 00:46:05 +0530

    remove browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { store } from './store/store.js'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import App from './App.jsx'
 import ScrollToTop from "./constants/ScrollToTop.jsx"
+import './index.css'
+import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <HashRouter>
